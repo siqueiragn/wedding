45cae40931d7e7393bd4cfc301a00a4106e60e32
Gabriel <gabrielndesiqueira@hotmail.com>
2026-03-22 17:27:29 -0300

    feat: links

--- lista_presentes.php
+++ lista_presentes.php
@@ -1,3 +1,36 @@
+<?php 
+
+//Primeira Linha
+$lista['so_para_n_dizerem']         = 'https://pag.ae/81CdzWtCG';
+$lista['cafe_manha_noivos']         = 'https://pag.ae/81CdAXT9q';
+$lista['equipamento_noiva_dialogo'] = 'https://pag.ae/81CdBYG7L';
+$lista['equipamento_noivo_dialogo'] = 'https://pag.ae/81CdCHXB1';
+
+//Segunda Linha
+$lista['preferencia_buffet']        = 'https://pag.ae/81CdD-L2G';
+$lista['pedido_desculpas_noivo']    = 'https://pag.ae/81CdEZK65';
+$lista['semana_ifood_noiva']        = 'https://pag.ae/81CdGjFzq';
+$lista['equipamento_noiva_dormir']  = 'https://pag.ae/81CdH1aM1';
+
+//Terceira Linha
+$lista['kit_engov_noiva']            = 'https://pag.ae/81CdJbQB5';       
+$lista['ingresso_preferencial_bar']  = 'https://pag.ae/81CdJPHb5';               
+$lista['cota_noiva_n_jogar_buque']   = 'https://pag.ae/81CdKAR61';               
+$lista['cota_casal_perguntar_filho'] = 'https://pag.ae/81CdLaThL';                   
+
+//Quarta Linha
+$lista['ingresso_renovacao_votos'] = 'https://pag.ae/81CdQ42cG';
+$lista['mochila_para_noiva']       = 'https://pag.ae/81CdQTUQm';
+$lista['suite_para_noivo']         = 'https://pag.ae/81CdRYj2G';
+$lista['terapia_para_casal']       = 'https://pag.ae/81CdSGhTL';
+
+//Quinta Linha
+$lista['plano_b_dieta_noivos']   = 'https://pag.ae/81CdTBshL';
+$lista['deus_tocou_seu_coracao'] = 'https://pag.ae/81CdU8xw5';
+$lista['posso_levar_fulano']     = 'https://pag.ae/81CdVkazq';
+$lista['melhor_presente']        = 'https://pag.ae/81CdVUdpq';
+?>
+
 <div class="section-title position-relative text-center">
                 <h6 class="text-uppercase text-primary mb-3" style="letter-spacing: 3px;">Presentes</h6>
                 <h1 class="font-secondary display-4">Lista de Presentes</h1>
@@ -18,7 +51,7 @@
                             <p class="text-uppercase">Só para não dizerem que não dei nada. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
                             <h4 class="mb-3">R$ 100,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['so_para_n_dizerem']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>1/8</small>
                             </div>
@@ -33,7 +66,7 @@
                             <p class="text-uppercase">Um café da manhã reforçado para os noivos.</p>
                             <h4 class="mb-3">R$ 150,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['cafe_manha_noivos']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -48,7 +81,7 @@
                             <p class="text-uppercase">Equipamento para noiva dialogar com o noivo.</p>
                             <h4 class="mb-3">R$ 200,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['equipamento_noiva_dialogo']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>1/8</small>
                             </div>
@@ -62,7 +95,7 @@
                             <p class="text-uppercase">Equipamento para noivo usar durante diálogo com a noiva.</p>
                             <h4 class="mb-3">R$ 250,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['equipamento_noivo_dialogo']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -83,7 +116,7 @@
                         </p>
                             <h4 class="mb-3">R$ 280,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['preferencia_buffet']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -98,7 +131,7 @@
                             <p class="text-uppercase">Kit de pedido de desculpas mesmo sem o noivo estar errado.</p>
                             <h4 class="mb-3">R$ 300,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['pedido_desculpas_noivo']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -113,7 +146,7 @@
                             <p class="text-uppercase">Uma semana de Ifood para a noiva não precisar comer a comida do noivo.</p>
                             <h4 class="mb-3">R$ 350,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['semana_ifood_noiva']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -128,7 +161,7 @@
                             <p class="text-uppercase">Equipamento para noiva dormir sem ouvir os roncos do noivo.</p>
                             <h4 class="mb-3">R$ 380,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['equipamento_noiva_dormir']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -148,7 +181,7 @@
                             <p class="text-uppercase">KIT de ENGOV para a noiva consumir depois da festa.</p>
                             <h4 class="mb-3">R$ 400,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['kit_engov_noiva']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -163,7 +196,7 @@
                             <p class="text-uppercase">Ingresso preferencial para o bar de drinks.</p>
                             <h4 class="mb-3">R$ 450,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['ingresso_preferencial_bar']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -178,7 +211,7 @@
                             <p class="text-uppercase">Cota para a noiva não jogar o buquê no seu par.</p>
                             <h4 class="mb-3">R$ 500,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['cota_noiva_n_jogar_buque']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -193,7 +226,7 @@
                             <p class="text-uppercase">Cota para perguntar quando o casal vai ter filhos.</p>
                             <h4 class="mb-3">R$ 600,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['cota_casal_perguntar_filho']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -214,7 +247,7 @@
                             <p class="text-uppercase">Ingresso antecipado para a renovação de votos.</p>
                             <h4 class="mb-3">R$ 650,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['ingresso_renovacao_votos']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -229,7 +262,7 @@
                             <p class="text-uppercase">Mochila para a noiva usar na viagem de lua de mel.</p>
                             <h4 class="mb-3">R$ 700,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['mochila_para_noiva']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -244,7 +277,7 @@
                             <p class="text-uppercase">Suíte para o noivo dormir quando incomodar a noiva.</p>
                             <h4 class="mb-3">R$ 800,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['suite_para_noivo']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -259,7 +292,7 @@
                             <p class="text-uppercase">Terapia para o casal após o casamento (vamos precisar).</p>
                             <h4 class="mb-3">R$ 900,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['terapia_para_casal']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -279,7 +312,7 @@
                             <p class="text-uppercase">Plano B para a dieta dos noivos após o casamento.</p>
                             <h4 class="mb-3">R$ 1.000,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['plano_b_dieta_noivos']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -293,7 +326,7 @@
                             <p class="text-uppercase">Deus tocou seu coração e você quer ajudar os noivos.</p>
                             <h4 class="mb-3">R$ 1.500,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['deus_tocou_seu_coracao']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -308,7 +341,7 @@
                             <p class="text-uppercase">"Posso levar fulano?" &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
                             <h4 class="mb-3">R$ 1.785,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['posso_levar_fulano']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
@@ -324,7 +357,7 @@
                             <p class="text-uppercase">Só pra dizer que dei o melhor presente. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
                             <h4 class="mb-3">R$ 2.000,00</h4>
                             <div class="d-inline-block">
-                                <button class="btn btn-primary font-weight-bold py-2 px-3" type="submit">Comprar</button>
+                                <a href="<?php echo $lista['melhor_presente']; ?>" class="btn btn-primary font-weight-bold py-2 px-3" target="_blank">Comprar</a>
                                 <br class="clear">
                                 <small>0/8</small>
                             </div>
