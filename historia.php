8c42fddfc6f425746e52bc673be7536ffb19a8dc
Gabriel <gabrielndesiqueira@hotmail.com>
2026-05-18 09:06:08 -0300

    fix:

---
+++ historia.php
@@ -0,0 +1,80 @@
+<div class="section-title position-relative text-center">
+                <h6 class="text-uppercase text-primary mb-3" style="letter-spacing: 3px;">História</h6>
+                <h1 class="font-secondary display-4">Nossa História</h1>
+                <i class="far fa-heart text-dark"></i>
+            </div>
+            <div class="container timeline position-relative p-0">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-right">
+                        <img class="img-fluid mr-md-3" src="img/hist-1.png" alt="" style="height: unset !important";>
+                    </div>
+                    <div class="col-md-6 text-center text-md-left">
+                        <div class="h-100 d-flex flex-column justify-content-center bg-secondary p-4 ml-md-3">
+                            <h4 class="mb-2">Primeiros momentos</h4>
+                            <p class="text-uppercase mb-2">02/01/2018</p>
+                            <p class="m-0">No segundo dia de 2018, Gabriel começou a trabalhar com a Natália, aí bateu os olhos nela e se apaixonou.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-right">
+                        <div class="h-100 d-flex flex-column justify-content-center bg-secondary p-4 mr-md-3">
+                            <h4 class="mb-2">Primeira festa juntos</h4>
+                            <p class="text-uppercase mb-2">20/01/2019</p>
+                            <p class="m-0">No dia 20 de janeiro de 2019, Gabriel e Natália foram para a primeira festa juntos, um open bar em Cachoeirinha. Naquela noite construíram uma amizade muito grande que só cresceu nos próximos anos. Naquela noite Gabriel já estava encantado pela Nati.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 text-center text-md-left">
+                        <img class="img-fluid ml-md-3" src="img/hist-2.png" alt="" style="height: unset !important">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-right">
+                        <img class="img-fluid mr-md-3" src="img/hist-3.jpg" alt="" style="height: unset !important">
+                    </div>
+                    <div class="col-md-6 text-center text-md-left">
+                        <div class="h-100 d-flex flex-column justify-content-center bg-secondary p-4 ml-md-3">
+                            <h4 class="mb-2">Viagem Guarda do Embaú </h4>
+                            <p class="text-uppercase mb-2">05/03/2019</p>
+                            <p class="m-0">Gabriel convidou a Nati para ir em uma viagem para a Guarda do Embaú em SC, onde fizeram trilhas e aproveitaram um lugar incrível juntos..</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-right">
+                        <div class="h-100 d-flex flex-column justify-content-center bg-secondary p-4 mr-md-3">
+                            <h4 class="mb-2">Primeiro Beijo</h4>
+                            <p class="text-uppercase mb-2">02/2021</p>
+                            <p class="m-0">(Sim, levou 3 anos) Viajaram para Macaé/RJ para fazer implantação do sistema em um cliente da empresa e evoluíram o sentimento, rolando até mesmo o primeiro beijo entre noites de longas conversas e muito trabalho. Porém, acabaram se afastando após a viagem.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 text-center text-md-left">
+                        <img class="img-fluid ml-md-3" src="img/hist-4.jpg" alt="" style="height: unset !important">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-right">
+                        <img class="img-fluid mr-md-3" src="img/hist-5.jpg" alt="" style="height: unset !important">
+                    </div>
+                    <div class="col-md-6 text-center text-md-left">
+                        <div class="h-100 d-flex flex-column justify-content-center bg-secondary p-4 ml-md-3">
+                            <h4 class="mb-2">Pedido de Namoro</h4>
+                            <p class="text-uppercase mb-2">25/08/2023</p>
+                            <p class="m-0">Após se reencontrarem (novamente em uma viagem de trabalho), o destino colocou os dois juntos novamente e dessa vez decididos a encarar o relacionamento.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-right">
+                        <div class="h-100 d-flex flex-column justify-content-center bg-secondary p-4 mr-md-3">
+                            <h4 class="mb-2">Pedido de Casamento</h4>
+                            <p class="text-uppercase mb-2">14/04/2024</p>
+                            <p class="m-0">Após 8 meses juntos e cheios de amor, Gabriel pede Natália em casamento em uma noite especial. O tempo que demorou pra oficializarem um namoro, compensou no pedido de casamento.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 text-center text-md-left">
+                        <img class="img-fluid ml-md-3" src="img/hist-6.jpg" alt="" style="height: unset !important">
+                    </div>
+                </div>
+            </div>
---
+++ roteiro.php
@@ -0,0 +1,54 @@
+<div class="section-title position-relative text-center">
+                <h6 class="text-uppercase text-primary mb-3" style="letter-spacing: 3px;">Roteiro</h6>
+                <h1 class="font-secondary display-4">Roteiro do dia</h1>
+                <i class="far fa-heart text-dark"></i>
+            </div>
+            <div class="row portfolio-container">
+            
+                <div class="col-lg-6 col-md-6 offset-lg-3 offset-xs-3 portfolio-item">
+                    <div class="position-relative mb-2">
+                        <img class="img-fluid w-100" src="img/roteiro/zanfir.png" alt="">
+                        <div class="bg-secondary text-center p-4">
+                            <h4 class="mb-3">Chef Zanfir</h4>
+                            <p class=""><a href="https://instagram.com/chefzanfirgastronomia_eventos" target="_blank">@chefzanfirgastronomia_eventos</a><br><br>
+
+                            Com mais de 18 anos de experiência, o Chef é um importante colunista gastronômico da região. Estará presente junto com sua equipe para fazer o buffet e cuidar da alimentação de todos os convidados. Teremos entradinhas, janta, sobremesas e lanche da madrugada.
+                            </p>
+                            <!-- <div class="d-inline-block">
+                                <a class="mx-2" href="#"><i class="fab fa-twitter"></i></a>
+                                <a class="mx-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a class="mx-2" href="#"><i class="fab fa-instagram"></i></a>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>  
+                <br class="clear">
+                <br class="clear">
+                <div class="col-lg-6 col-md-6 offset-lg-3 offset-xs-3 portfolio-item">
+                    <div class="position-relative mb-2">
+                        <img class="img-fluid w-100" src="img/roteiro/openbar.jpg" alt="">
+                        <div class="bg-secondary text-center p-4">
+                            <h4 class="mb-3">Operação OpenBar</h4>
+                            <p class=""><a href="https://instagram.com/operacaoproducoes" target="_blank">@operacaoproducoes</a><br><br>
+
+                            A Operação OpenBar leva drinks para eventos há mais de 12 anos. Serão 3 barmans oferecendo 6 tipos de drinks:
+                            <ul style="text-align: left;">
+                                <li>Bramble (drink da noiva) </li>
+                                <li>London Mule (drink do noivo)</li>
+                                <li>Whiskey Sour (drink do casal)</li>
+                                <li>Aperol Spritz</li>
+                                <li>Mojito</li>
+                                <li>Piña Colada.
+                            </ul>
+                            E também mais de 40 combinações de caipirinhas (morango, limão, kiwi, maracujá e abacaxi) começando às 22h e indo até 3h.
+                            </p>
+                            <!-- <div class="d-inline-block">
+                                <a class="mx-2" href="#"><i class="fab fa-twitter"></i></a>
+                                <a class="mx-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a class="mx-2" href="#"><i class="fab fa-instagram"></i></a>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>  
+                
+            </div>
